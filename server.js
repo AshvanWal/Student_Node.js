1b7a0a38c17e964e4ef28dadef19eb7b0844b8ea
Ashvan <ashvan.wal@gmail.com>
2019-03-27 09:51:42 -0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const hbs = require('hbs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('message', (text) => {
+    return text.toUpperCase();
+})
+
+app.get('/', (request, response) => {
+    response.send({
+        name: 'Your Name',
+        school: [
+            'BCIT',
+            'SFU',
+            'UBC'
+        ]
+    })
+});
+
+
+app.get('/info', (request, response) => {
+    response.render('about.hbs', {
+        title: 'About page',
+        // year:  new Date().getFullYear()
+        welcome: 'Hello!'
+    });
+});
+
+app.get('/404', (request, response) => {
+    response.send({
+        error: 'Page not found'
+    })
+});
+
+
+app.listen(8080, () => {
+    console.log('Server is up on the port 8080');
+});
